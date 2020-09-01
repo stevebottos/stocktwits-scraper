cb8f84d895d97e8aab5f6363eceb6044b9066ffe
stevebottos <steve.bottos@hotmail.ca>
2020-08-31 17:32:08 -0700

    ADD watchlist functionality

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
+"""
+* Needed to enable gmails "less secure apps" to allow the email portion to work
+"""
+
+import smtplib
+import ssl
+from email.mime.text import MIMEText
+import os
+import secrets
 import requests
 import json
 
+WATCHLIST = [
+"TSLA",
+"NAK",
+"RKT",
+"GEVO",
+"MARA",
+"BLIN",
+"OPTT",
+"EMAN",
+"LAC",
+"AKER",
+"SINT",
+"RNET",
+"APEX",
+]
 
-def scrape(users_of_interest):
+USERS = [
+    "newsfilterio",
+    "fla"
+]
 
+def scrape(users_of_interest, tickers_of_interest):
+
+    scraped_messages = []
     url_str = "https://api.stocktwits.com/api/2/streams/user/{}.json"
 
     for user in users_of_interest:
@@ -14,12 +44,36 @@ def scrape(users_of_interest):
         messages = parsed["messages"]
 
         for message in messages:
-            print(json.dumps(message["body"], indent=4, sort_keys=True))
+            message_body = message["body"]
+            for ticker in tickers_of_interest:
+                search_key = "".join(("$", ticker))
+                if search_key in message_body:
+                    scraped_messages.append(message_body)
 
 
-users_of_interest = [
-    "newsfilterio",
-    "fla"
-]
+    return scraped_messages
 
-scrape(users_of_interest)
+
+def format_email_message(email_content):
+    return "\n---\n".join(email_content)
+
+
+def send_email(email_content):
+    msg = MIMEText(email_content)
+
+    msg['Subject'] = "STOCKTWITS ALERT"
+    msg['From'] = msg['To'] = secrets.USER
+
+    context=ssl.create_default_context()
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as smtp:
+        smtp.starttls(context=context)
+        smtp.login(secrets.USER, secrets.PASS)
+        smtp.send_message(msg)
+
+
+email_raw = scrape(USERS, WATCHLIST)
+
+if len(email_raw):
+    email_formatted = format_email_message(email_raw)
+    send_email(email_formatted)
